388c6e7ff37dc5591459ab3cec57cc0ba176fe4b
sammid37 <samanthadmedeiros37@gmail.com>
2024-03-03 22:10:08 -0300

    Update lexer, syntax e main
    Co-authored-by: EnthonyMiguel <EnthonyMiguel@users.noreply.github.com>

--- test/syntax_tests/Test4.pas
+++ test/syntax_tests/Test4.pas
@@ -2,12 +2,12 @@ program Test4;
 var
    A, B, R, I : integer;
 
-procedure teste (A:integer, B:real);
+procedure teste (A:integer; B:real);
 var
    S,X: real;
 begin
-   S := A + B * X
-end  {verifique se é necessário um ";" no fechamento de um procedimento}
+   S := A + B * X;
+end;  {verifique se é necessário um ";" no fechamento de um procedimento}
 
 begin
    while (I <= 5) do
@@ -15,8 +15,8 @@ begin
       A := A+1;
       B := B-1;
       R := A + B;
-      I := I + 1
-   end
+      I := I + 1;
+   end;
 end.
 
 {retirar algumas palavras reservadas para gerar erros sintáticos}
