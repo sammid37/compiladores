b55e95b2cd9318a61d3c99c7293ee3d6620901c7
sammid37 <samanthadmedeiros37@gmail.com>
2024-04-07 09:29:13 -0300

    📄 Identificador Tipado: implementação de classe
    Co-authored-by: EnthonyMiguel <EnthonyMiguel@users.noreply.github.com>

--- identificador_tipado.py
+++ identificador_tipado.py
@@ -1,3 +1,7 @@
+# Construção de Compiladores
+# Implementação de classe/tipo Identificador Tipado
+# Enthony e Samantha
+
 class IdentificadorTipado:
   def __init__(self, identificador, tipo):
     self.identificador = identificador
